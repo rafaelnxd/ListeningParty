10f3aa40babfb28630394a8831438d3ff9985d46
rafaelnxd <rafaelnovaes46@outlook.com>
2024-02-24 18:00:52 -0300

    generate code function

--- listening_party/api/models.py
+++ listening_party/api/models.py
@@ -1,9 +1,27 @@
 from django.db import models
+import string
+import random
+
+def generate_unique_code():
+    length = 5
+    
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
 
 # Create your models here.
 class Room(models.Model):
-    room_code = models.CharField(max_length=8, default="", unique=True)
+    room_code = models.CharField(max_length=5, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
