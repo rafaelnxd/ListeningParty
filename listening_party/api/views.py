96352fdb0d4247d401f75c97c9e2e26889ecdf0a
rafaelnxd <rafaelnovaes46@outlook.com>
2024-02-24 18:16:20 -0300

    roomview and roomserializer

--- listening_party/api/views.py
+++ listening_party/api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+
 
 # Create your views here.
 
-def main(request):
-    return HttpResponse("<h1>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>")
+class RoomView(generics.CreateAPIView):    ###show all rooms on database
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+
 
